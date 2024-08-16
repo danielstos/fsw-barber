961708f534cd758a6e2b2f5ffa254b1c2315f63f
danielstos <daniel.jose.stos@gmail.com>
2024-08-16 19:59:02 -0300

    feat: ensure only signed in user sign out

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -80,17 +80,18 @@ const SidebarSheet = () => {
           </SheetClose>
         ))}
       </div>
-
-      <div className="flex flex-col gap-2 py-5">
-        <Button
-          variant="ghost"
-          className="justify-start gap-2"
-          onClick={handleLogoutClick}
-        >
-          <LogOutIcon size={18} />
-          Sair da conta
-        </Button>
-      </div>
+      {data?.user && (
+        <div className="flex flex-col gap-2 py-5">
+          <Button
+            variant="ghost"
+            className="justify-start gap-2"
+            onClick={handleLogoutClick}
+          >
+            <LogOutIcon size={18} />
+            Sair da conta
+          </Button>
+        </div>
+      )}
     </SheetContent>
   )
 }
