af0a52a82023542d8f90d990bd220fe976ead31b
danielstos <daniel.jose.stos@gmail.com>
2024-08-17 12:05:27 -0300

    feat: ensure only authenticated user can book

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -136,7 +136,6 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
 
       await createBooking({
         serviceId: service.id,
-        userId: (data?.user as any).id,
         date: newDate,
       })
 
