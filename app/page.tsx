7e1ed96cce7774bcd26b51a83f4954580a70d39a
danielstos <daniel.jose.stos@gmail.com>
2024-08-21 00:42:41 -0300

    fix:  add images to README

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,10 @@ const Home = async () => {
         {/* Agendamento */}
         <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {confirmedBookings.map((booking) => (
-            <BookingItem key={booking.id} booking={booking} />
+            <BookingItem
+              key={booking.id}
+              booking={JSON.parse(JSON.stringify(booking))}
+            />
           ))}
         </div>
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
